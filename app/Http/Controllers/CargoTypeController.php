de40a482bb56675d686ee8aab096bc2c2c23a3aa
The Creator <moralesjhon03@gmail.com>
2024-02-12 16:53:46 +0800

    init

---
+++ app/Http/Controllers/CargoTypeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CargoType;
+use App\Http\Requests\StoreCargoTypeRequest;
+use App\Http\Requests\UpdateCargoTypeRequest;
+
+class CargoTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCargoTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CargoType $cargoType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CargoType $cargoType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCargoTypeRequest $request, CargoType $cargoType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CargoType $cargoType)
+    {
+        //
+    }
+}
